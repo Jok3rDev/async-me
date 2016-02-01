37d19df8c8f5ee60e3bbef2d6bf734a29ef5be95
Fab. @Devtalk <contact@devtalk.fr>
2016-02-01 15:04:17 +0100

    Création du fichier principal

---
+++ jquery.aync-me.js
@@ -0,0 +1,9 @@
+/**
+ * jQuery Aynsc Me
+ * Un plug-in jQuery pour charger de manière asynchrone vos contenu multimédia.
+ * http://github.com/Jok3rDev/async-me
+ *
+ * Licensed under the MIT license.
+ * Copyright 2016 Fabrice Trimech
+ * https://github.com/Jok3rDev/
+ */
