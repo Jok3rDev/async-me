53012ece24e0e3c74aa69cc24c4850099f8be6bd
Fab. @Devtalk <contact@devtalk.fr>
2016-02-02 09:51:30 +0100

    Add promptError method

--- jquery.async-me.js
+++ jquery.async-me.js
@@ -11,28 +11,54 @@
 (function($) {
   'use strict';
 
-  var AsyncMe = function (options) {
+  var AsyncMe = function (element, options) {
+    this.$element = $(element);
+    this.options  = $.extend({}, $.fn.asyncMe.defaults, this.$element.data(), options);
+  };
 
+  AsyncMe.prototype.checkAttribute = function (element) {
+    var check    = true;
+    var $element = element;
+
+    check &= Boolean($element.data('src') === undefined);
+    check &= Boolean($element.data('src') != '');
+
+    if (check) {
+      this.promptError(1);
+    }
+    return check;
+  };
+
+  AsyncMe.prototype.promptError = function (id) {
+    var ret = null;
+    switch (id) {
+      case 1:
+        ret = "L'attribut data-src n'est pas correctement formaté.";
+      break;
+      default:
+        ret = "Une erreur est survenue.";
+    }
+    return console.error(ret);
   };
 
   var old = $.fn.asyncMe;
   $.fn.asyncMe = AsyncMe;
 
   $.fn.asyncMe = function (option, args) {
-      return this.each(function () {
-          var $this = $(this)
+    return this.each(function () {
+      var $this = $(this)
       , data = $this.data('asyncMe')
       , options = typeof option === 'object' && option;
 
-    if(!data) $this.data( 'asyncMe', (data = new AsyncMe(this, options)));
-    if( typeof option === 'string' && option == 'text' ){ data.setText( args ) }
-      });
+      if(!data) $this.data( 'asyncMe', (data = new AsyncMe(this, options)));
+      if( typeof option === 'string' && option == 'text' ){ data.setText( args ) }
+    });
   }
 
   $.fn.asyncMe.Constructor = AsyncMe;
 
   $.fn.asyncMe.defaults = {
-
+    timeOut : 500,
   }
 
   $.fn.asyncMe.noConflict = function () {
