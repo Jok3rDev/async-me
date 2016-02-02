1482a45210b1c5fad35c1109e1ca3778a8e0d2c0
Fab. @Devtalk <contact@devtalk.fr>
2016-02-02 09:22:39 +0100

    Rebase, project mis sur prototype

--- jquery.async-me.js
+++ jquery.async-me.js
@@ -1,55 +1,43 @@
 /**
- * jQuery Async Me
- * Un plug-in jQuery pour charger de manière asynchrone vos contenu multimédia.
- * http://github.com/Jok3rDev/async-me
- *
- * Licensed under the MIT license.
- * Copyright 2016 Fabrice Trimech
- * https://github.com/Jok3rDev/
- */
+* jQuery Async Me
+* Un plug-in jQuery pour charger de manière asynchrone vos contenu multimédia.
+* http://github.com/Jok3rDev/async-me
+*
+* Licensed under the MIT license.
+* Copyright 2016 Fabrice Trimech
+* https://github.com/Jok3rDev/
+*/
 
 (function($) {
-  $.fn.asyncMe = function() {
-    'use strict';
-    console.log('Le plug-in est exécuté');
-    var $element = this;
-    console.log($element);
-    $element.each(function(key, value){
-      checkAttribute($(this));
-      defineElement(value.tagName);
-    });
-    return this;
+  'use strict';
 
-    /**
-     * Vérifie si l'attribut data-src est présent et non null
-     * @param  $element L'élement du DOM sur lequel est exécuté AsyncMe
-     * @return booléen
-     */
-    function checkAttribute($element) {
-      var check = true;
-      check &= Boolean($element.data('src') === undefined);
-      check &= Boolean($element.data('src') != '');
+  var AsyncMe = function (options) {
 
-      if (check) {
-        console.error("L'attribut data-src n'est pas correctement formaté.");
-      }
-      return check;
-    }
+  };
 
-    function defineElement(tag) {
-      var tag    = tag.toLowerCase();
-      var result = [];
+  var old = $.fn.asyncMe;
+  $.fn.asyncMe = AsyncMe;
 
-      switch (tag) {
-        case 'img':
-        case 'iframe':
-          result['toChange'] = 'src';
-          break;
-        default:
-        console.error("L'élement du DOM définit n'est pas géré par le plug-in.");
-        result = false;
-      }
-      return result;
-    }
+  $.fn.asyncMe = function (option, args) {
+      return this.each(function () {
+          var $this = $(this)
+      , data = $this.data('asyncMe')
+      , options = typeof option === 'object' && option;
+
+    if(!data) $this.data( 'asyncMe', (data = new AsyncMe(this, options)));
+    if( typeof option === 'string' && option == 'text' ){ data.setText( args ) }
+      });
   }
+
+  $.fn.asyncMe.Constructor = AsyncMe;
+
+  $.fn.asyncMe.defaults = {
+
+  }
+
+  $.fn.asyncMe.noConflict = function () {
+    $.fn.asyncMe = old;
+    return this;
+  }
+
 })(window.jQuery);
